45c04ae467f4046b730178e5bb2310e37baf9a10
zhipengwei <zhipengwei.sunshine@gmail.com>
2017-01-22 09:03:32 -0800

    Put those files under tracking

---
+++ config.h
@@ -0,0 +1,6 @@
+#define EXPERIMENT_CONFIG_BUFFER_SIZE_PACKETS 10
+#define EXPERIMENT_CONFIG_BUFFER_SIZE_BYTES 100000
+#define EXPERIMENT_CONFIG_START_TIME   1 
+#define EXPERIMENT_CONFIG_STOP_TIME    10
+#define EXPERIMENT_CONFIG_PACKET_SIZE 10 // This is the parameters of the distribution of packet size
+#define EXPERIMENT_CONFIG_INTERVAL    10 // This is the parameters of the distribution of packet interval
